a37b9c28e2d91fb1ea61ac2d58a19132c56cf3d6
eJwtzMFqhDAQgOF3mXMOjdqouZWFUsr2UCg99BLGcbRZo0kTbSrLvnth8frD918BiTilDz/xAhoIH2vkRsmmVEpJqnhou+KhxlIyVVRXDRVKMoKAEP2v7Tke9HqczHqsxm9vTvnlK9d0Ppd9K1/X55999n0+je3wVJSf8vLWvl9AwJ2YdQ8MGjrGyBEEJPL3gP1sFx22zlkyE+8iMvbax1FEDl5siaPmGa0T2cdpcD6DAP4LNnIyuIJeNudut39NKU/3 <gerard.doblas@enti.cat>
2025-05-06 17:39:38 +0200

    FileReader implemented

---
+++ DoblasGerard_ZornozaHector_AA2_1/DoblasGerard_ZornozaHector_AA2_1/FileReader.cpp
@@ -0,0 +1,25 @@
+#include "FileReader.h"
+
+bool FileReader::ReadSettings(const string& filename, Settings& settings) 
+{
+	fstream myFile(filename);
+    if (!myFile.is_open()) {
+        ComprobarFichero();
+        exit(1);
+    }
+
+    char aux;
+    myFile >> settings.FILAS >> aux >> settings.COLUMNAS >> aux
+        >> settings.SANTOS_PEDESTRIANS_NUMBER >> aux >> settings.SANTOS_MONEY_REQUIRED >> aux >> settings.SANTOS_MAX_MONEY >> aux
+        >> settings.FIERRO_PEDESTRIANS_NUMBER >> aux >> settings.FIERRO_MONEY_REQUIRED >> aux >> settings.FIERRO_MAX_MONEY >> aux;
+
+    myFile.close();
+    return true;
+}
+
+void FileReader::ComprobarFichero() {
+    std::cout << "Lamentablemente, ha surgido un inconveniente al intentar abrir el archivo 'config.txt'.\n"
+        << "Por favor, tómese un momento para verificar que el archivo exista y contenga los datos necesarios.\n"
+        << "Si el problema persiste, no dude en comunicarse con el administrador del sistema para recibir asistencia adicional.\n"
+        << "¡Gracias por su comprensión y colaboración!\n";
+}
---
+++ DoblasGerard_ZornozaHector_AA2_1/DoblasGerard_ZornozaHector_AA2_1/FileReader.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+struct Settings
+{
+public:
+	int FILAS;
+	int COLUMNAS;
+	int SANTOS_PEDESTRIANS_NUMBER;
+	int SANTOS_MONEY_REQUIRED;
+	int SANTOS_MAX_MONEY;
+	int FIERRO_PEDESTRIANS_NUMBER;
+	int FIERRO_MONEY_REQUIRED;
+	int FIERRO_MAX_MONEY;
+};
+
+class FileReader
+{
+public:
+	bool ReadSettings(const string& filename, Settings& settings);
+private:
+	void ComprobarFichero();
+};
