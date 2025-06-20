cd3b0305f8cecf6a81abed80b9dbec964decacf0
HectorZ70 <hector.zornoza@enti.cat>
2025-06-20 16:37:17 +0200

    Main Loop

--- DoblasGerard_ZornozaHector_AA2_1/DoblasGerard_ZornozaHector_AA2_1/Main.cpp
+++ DoblasGerard_ZornozaHector_AA2_1/DoblasGerard_ZornozaHector_AA2_1/Main.cpp
@@ -125,21 +125,22 @@ int main()
         case GameScenes::GAMEPLAY:
         {
             cj.MoveCJ(m.limiteMov_X, m.limiteMov_Y, m, p, settings, c);
+            if (cj.money >= settings.FIERRO_MONEY_REQUIRED && m.box[cj.pos.y][cj.pos.x] == Boxes::PEAJE)
+            {
+                m.UnlockFierro();
+
+            }
+            else if (m.box[cj.pos.y][cj.pos.x] == Boxes::PEAJE)
+            {
+                exit(0);
+            }
             m.box[cj.prevPos.y][cj.prevPos.x] = static_cast<Boxes>(Boxes::VACIO);
             m.box[cj.pos.y][cj.pos.x] = static_cast<Boxes>(cj.CJLook);
 
             p.WalkerManagment(cj.pos.x, cj.pos.y, m, settings);
             c.CarsManagment(m, settings);
             //Aqui va la funcion management del boss
-            if (cj.money >= settings.FIERRO_MONEY_REQUIRED && m.box[cj.pos.y][cj.pos.x] == Boxes::PEAJE)
-            {
-                m.UnlockFierro();
-
-            }
-            else if(m.box[cj.pos.y][cj.pos.x] == Boxes::PEAJE)
-            {
-               exit(0);
-            }
+            
 
             m.PintarVista(cj.pos);
             Sleep(1000 / MAX_NUM_FPS);
@@ -147,8 +148,32 @@ int main()
 
             
         }
+        break;
 
+        case GameScenes::GAMEOVER:
+        {
+            system("cls");
+            std::cout << "\033[38;5;153m";
+            std::cout << R"()";
+            std::cout << "\033[0m";
+            std::cout << R"(
+   _____                       ____                 
+  / ____|                     / __ \                
+ | |  __  __ _ _ __ ___   ___| |  | |_   _____ _ __ 
+ | | |_ |/ _` | '_ ` _ \ / _ \ |  | \ \ / / _ \ '__|
+ | |__| | (_| | | | | | |  __/ |__| |\ V /  __/ |   
+  \_____|\__,_|_| |_| |_|\___|\____/  \_/ \___|_| 
+            
+)";
+            Sleep(5000);
+            currentScene = GameScenes::MAIN_MENU;
+        }
+            break;
+        default:
 
+            break;
         }
     }
+
+    return 0;
 }
